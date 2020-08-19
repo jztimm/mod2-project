d25c1b46fddccabde0c3ca0f6b205bf3d552cbf0
jztimm <jztimm@gmail.com>
2020-08-19 09:08:59 -0400

    jQuery & popover install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "boostrap"
+
+document.addEventListener("turbolinks:load", () => {
+   $('[daya-toggle="tooltip"]').tooltip()
+   $('[daya-toggle="tooltip"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

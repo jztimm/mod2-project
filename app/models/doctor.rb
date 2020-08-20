0fe92bde848cb3c2666613f0b8abd1aa26336944
jztimm <jztimm@gmail.com>
2020-08-19 23:43:41 -0400

    Updated validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,7 +5,7 @@ class Doctor < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :age, presence: true
-  validates :age, numericality: {greater_than: 25, less_than: 150}
+  validates :age, numericality: {greater_than: 21, less_than: 150}
   validates :specialty, presence: true
   validates :city, presence: true
   validates :zipcode, :length => { :minimum => 5, :maximum => 6 }, :numericality => true, allow_blank: true

6a89b6a2a0e668ce9c39105d62a8c3e3d06871df
woink <wardprice@gmail.com>
2020-08-21 07:40:06 -0400

    removed unneeded routes for doctors, USA format for birthdate creation, removing unneed code from doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :appointments, only: [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :appointments
   resources :patients, except: [:index]
-  # post 'patients/delete', to: 'patients#destroy', as: 'destroy_patient'
-
-  resources :doctors
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :doctors, only: [:index, :show]
   
   root 'doctors#index'
-  # get '/doctors/search', to: 'doctors#search', as: 'search_doctors'
   delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
   get '/sessions/new', to: 'sessions#new', as: 'new_login'
   post '/sessions/create', to: 'sessions#create', as: 'login'
